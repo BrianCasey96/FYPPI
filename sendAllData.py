92a3b0dac8fc8fa2f7985b2a2cbd627f72fc5b8d
Brian Casey <b.casey3@nuigalway.ie>
2018-01-21 00:33:29 +0000

    Added all sensors so that their values are sent to db using package pymysql

--- sendAllData.py
+++ sendAllData.py
@@ -1,21 +1,31 @@
 #! /usr/bin/env python
 from time import sleep
 from gpiozero import LightSensor
-import MySQLdb
 import spidev
+import pymysql.cursors
+import os
+import glob
 
-cnx = MySQLdb.connect(host="sql2.freemysqlhosting.net",
-                      user="sql2202637",
-                      passwd="aI7%wK3%",
-                      db="sql2202637")
+cnx = pymysql.connect(host="sql2.freemysqlhosting.net",
+                     user="sql2202637",
+                     passwd="aI7%wK3%",
+                     db="sql2202637")
 
-# Establish SPI device on Bus 0,Device 0
+# Establish SPI device on Bus 0,Device 0 for moisture reading
 spi = spidev.SpiDev()
 spi.open(0,0)
 
+#for temperature reading
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+
 ldr = LightSensor(14)
 
-def getAdc (channel):
+def getMositureLevel(channel):
     #check valid channel
     if ((channel>7)or(channel<0)):
         return -1
@@ -26,34 +36,55 @@ def getAdc (channel):
     #Filter data bits from returned bits
     adcOut = ((r[1]&3) << 8) + r[2]
     global percent
-    percent = int(round(adcOut/10.24))
-    sendValues(percent)
-
+    percent = int(100- round(adcOut/10.24))
     
     #print out 0-1023 value and percentage
-    print("ADC Output: {0:4d} Percentage: {1:3}%".format (adcOut,percent))
+    print("Moisture Level: {0:4d} Mositure Percentage: {1:3}%".format (adcOut,percent))
     sleep(1)
+    
 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+	
 
-def sendValues(values):
-    cur = cnx.cursor()
+def sendValues(mositure, temp, light):
+    try:
+        with cnx.cursor() as cursor:
+            sql = "INSERT INTO Date_and_Value VALUES (null, '%d', '%lf', '%.2f')" % (mositure, temp, light)
+            cursor.execute(sql)
+        cnx.commit()
 
-    sql = "INSERT INTO Test(Number) VALUE ('%d' )" % (percent)
-    cur.execute(sql)
-
-    #cur.execute("INSERT INTO `Test` (`Number`) VALUE('11')")
-    cnx.commit()
-
-    cur.execute("SELECT * FROM Test")
-    for row in cur.fetchall():
-        print(row[0])
-
-    cnx.close()
+    finally:
+        print("Values Sent")
 
 while True:
-    getAdc(0)
-    print(ldr.value)
-
+    try:
+        getMositureLevel(0)
+        lightPercentage = ldr.value*100
+        print("Light Level: %.2lf%%" % (lightPercentage))
+        print("Temperature: %.2lf°C." % (read_temp()))
+        
+        sendValues(percent, read_temp(), ldr.value)
+        print("\n")
+        sleep(1)
+        
+    except KeyboardInterrupt:
+        cnx.close()
+##        GPIO.cleanup()
 
 
 
