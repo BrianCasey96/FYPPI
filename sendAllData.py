11dee35015dafaee0c565572b62bc9c6ed7a3a79
BrianCasey96 <b.casey3@nuigalway.ie>
2018-03-27 13:55:35 +0000

    Moved password and username to db to different file outside of Github

--- sendAllData.py
+++ sendAllData.py
@@ -4,14 +4,23 @@ from time import sleep
 from gpiozero import LightSensor
 import pymysql.cursors
 import os
+import sys
 import glob
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import subprocess
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
+
+#import PySQLPool
 
 import temp
 import moisture_level
+
+user = config.user()
+passwd = config.returnPassword()
+db = config.db()
 
 #subprocess.call(['./cloudproxy.sh'])
 
@@ -23,10 +32,19 @@ def connectionStatus(client, userdata, flags, rc):
 clientName = "RPI"
 serverAddress = "35.198.67.227"
 cnx = pymysql.connect(host="127.0.0.1",
-		      user="root",
-		      passwd="butterfly",
-		      db="plant_data",
+		      user=user,
+		      passwd=passwd,
+		      db=db,
 		      port = 3307 )
+
+#connection = PySQLPool.getNewConnection(host="127.0.0.1",
+#                      user="root",
+#                      passwd="butterfly",
+#                      db="plant_data",
+#                      port = 3307)
+
+#query = PySQLPool.getNewQuery(connection)
+
 ##for water pump
 init = False
 
@@ -62,14 +80,15 @@ def messageDecoder(client, userdata, msg):
            print("Unknown message!")
 
 def sendValues(mositure, temp, light):
-    try:
+     try:
         with cnx.cursor() as cursor:
-            sql = "INSERT INTO pidata VALUES (null, '%d', '%lf', '%f')" % (mositure, temp, light)
-            cursor.execute(sql)
+           sql = "INSERT INTO pidata VALUES (null, '%d', '%lf', '%f')" % (mositure, temp, light)
+           cursor.execute(sql)
         cnx.commit()
 
-    finally:
+     finally:
         print("Values Sent")
+#  query.Query("INSERT INTO pidata VALUES (null, '%d', '%lf', '%f')" % (mositure, temp, light))
 
 mqttClient = mqtt.Client(clientName)
 
@@ -82,13 +101,15 @@ mqttClient.loop_start()
 
 def main():
 #    try:
-     print("ADC Output: {0:4d} Percentage: {1:3}%".format (adc_output,percent))
-     lightPercentage = ldr.value*100
-     print("Light Level: %.2lf%%" % (lightPercentage))
-     print("Temperature: %.2lf°C." % (temp.read_temp()))
-     sendValues(percent, temp.read_temp(), lightPercentage)
-     print("\n")
-     sleep(300)
+     while 1==1:
+     	print("ADC Output: {0:4d} Percentage: {1:3}%".format (adc_output,percent))
+	global lightPercentage
+     	lightPercentage = ldr.value*100
+     	print("Light Level: %.2lf%%" % (lightPercentage))
+     	print("Temperature: %.2lf°C." % (temp.read_temp()))
+     	sendValues(percent, temp.read_temp(), lightPercentage)
+     	print("\n")
+     	sleep(300)
 
 ##        if percent < 60:
 ##            pump_on()
